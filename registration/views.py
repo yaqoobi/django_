912324d380736503bf4a541d7d8a9e577107bafd
Yaqoob Yaghoubi <yaqoobyaghoubi@Yaqoobs-MacBook-Pro.local>
2023-07-05 16:08:56 +0100

    feat: add sign in and sign up views

--- registration/views.py
+++ registration/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def index(request):
+    return render(request, "registration/index.html")
+
+
 def login(request):
     return render(request, "registration/login.html")
 
 
 def register(request):
     return render(request, "registration/register.html")
+
+
+def logout(request):
+    return render(request, "registration/logout.html")
